70d0511ed0144616784efd80d0ccd401c39ffc6e
the-krushnakant <krushnabhattad15@gmail.com>
2020-09-23 22:40:35 +0530

    Add files via upload

--- speed.py
+++ speed.py
@@ -4,10 +4,10 @@ from polynomial_regression.polyregress import polynomial_regression as pr
 import time 
 
 
-n=60000
+n=1000000
 deg=1
-a=[i%10000 for i in range(n)]
-b=[i**2+2*i+3 for i in a]
+a=[i%12 for i in range(n)]
+b=[i**deg+2*i+3 for i in a]
 
 x=np.asarray(a)
 y=np.asarray(b)
@@ -16,18 +16,22 @@ pr_regress_time=[]
 np_polyfit_time=[]
 np_polynomial_time=[]
 
-for i in range(200):
+for i in range(50):
     t1=time.time()
     c=pr.regress(x,y,deg)
     t2=time.time()
     pr_regress_time.append(t2-t1)
 
-for i in range(200):
+for i in range(50):
     t1=time.time()
     c=np.polyfit(x,y,deg)
     t2=time.time()
     np_polyfit_time.append(t2-t1)
 
+a=np.median(pr_regress_time)
+b=np.median(np_polyfit_time)
+ratio=b/a 
+print(a, b, ratio)
 
 '''
 x=np.linspace(1,200,num=200)
